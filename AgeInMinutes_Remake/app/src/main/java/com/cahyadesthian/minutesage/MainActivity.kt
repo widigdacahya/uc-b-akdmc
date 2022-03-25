b5e702b5efa3a1036e92dd23a4f1c61fdf27acbd
widigdacahya <rizkiwidigda@gmail.com>
2022-03-25 21:02:23 +0700

    remake init ageInMinute

---
+++ AgeInMinutes_Remake/app/src/main/java/com/cahyadesthian/minutesage/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.cahyadesthian.minutesage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cahyadesthian.minutesage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+    }
+}
