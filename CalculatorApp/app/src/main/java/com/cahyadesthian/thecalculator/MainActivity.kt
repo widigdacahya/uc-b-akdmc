0047a3421835539f012d0671254305c49dec7b80
widigdacahya <rizkiwidigda@gmail.com>
2022-03-25 16:19:23 +0700

    digit button and clear button function

--- CalculatorApp/app/src/main/java/com/cahyadesthian/thecalculator/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/cahyadesthian/thecalculator/MainActivity.kt
@@ -3,14 +3,25 @@ package com.cahyadesthian.thecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tv_input)
     }
 
-    fun onDigit(view: View) {
+    fun onDigitBtn(view: View) {
+        tvInput?.append((view as Button).text)
+    }
 
+    fun onClearBtn(view: View) {
+        tvInput?.text = ""
     }
 }
