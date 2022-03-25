ca8a3069f348d3f4764945e36a1d8d4e0ae7a0ab
widigdacahya <rizkiwidigda@gmail.com>
2022-03-25 16:12:07 +0700

    UI Calculator DONE

--- CalculatorApp/app/src/main/java/com/cahyadesthian/thecalculator/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/cahyadesthian/thecalculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.cahyadesthian.thecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+
     }
 }
