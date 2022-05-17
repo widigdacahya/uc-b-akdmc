a83db599f56c0ba35af94e0278d2c02beb65b1a5
widigdacahya <rizkiwidigda@gmail.com>
2022-05-17 11:03:47 +0700

    layout to start quiz
    card view to type name and button to start

--- QuizApp/app/src/main/java/com/cahyadesthian/thequiz/MainActivity.kt
+++ QuizApp/app/src/main/java/com/cahyadesthian/thequiz/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.cahyadesthian.thequiz
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cahyadesthian.thequiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        bg_animate()
+    }
+
+    private fun bg_animate() {
+        val animationDrawable: AnimationDrawable = mainBinding.bgMain.background as AnimationDrawable
+        animationDrawable.apply {
+            setEnterFadeDuration(1000)
+            setExitFadeDuration(3000)
+            start()
+        }
     }
 }
